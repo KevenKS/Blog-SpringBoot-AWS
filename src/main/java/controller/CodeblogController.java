1112fbd3a899f5dcdd1ffc8723fd01608ad46677
Keven <keven.sp@hotmail.com>
2021-03-24 14:56:33 -0300

    Criando controller

---
+++ src/main/java/controller/CodeblogController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import model.Post;
+import service.CodeblogService;
+
+@Controller
+public class CodeblogController {
+
+	@Autowired
+	CodeblogService	codeblogservice;
+	
+	@RequestMapping(value = "/posts", method = RequestMethod.GET)
+	public ModelAndView getPosts() {
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = codeblogservice.findAll();
+		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+	
+}
